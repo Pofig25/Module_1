134e8c1f9631a4408fef5ccb783f407f696846a1
Pofig25 <barmindm@gmail.com>
2024-10-29 18:27:03 +0300

    Add files via upload

---
+++ Module_1.py
@@ -0,0 +1,12 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+s = sorted(set(students))
+zipped = zip(s, grades)
+students_grades = dict(zipped)
+#print(students_grades)
+students_grades.update({'Aaron':sum(students_grades['Aaron']) / len(students_grades['Aaron']),
+                        'Bilbo':sum(students_grades['Bilbo']) / len(students_grades['Bilbo']),
+                        'Johnny':sum(students_grades['Johnny']) / len(students_grades['Johnny']),
+                        'Khendrik':sum(students_grades['Khendrik']) / len(students_grades['Khendrik']),
+                        'Steve':sum(students_grades['Steve']) / len(students_grades['Steve'])})
+print(students_grades)
